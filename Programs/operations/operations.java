9d207ff84d98083b329269f7c07bbbdce60c045f
Tejdeep Nookala <63906769+Tx-3011@users.noreply.github.com>
2024-07-23 10:36:26 +0530

    Initial Commit and First Day Commit

---
+++ Programs/operations/operations.java
@@ -0,0 +1,11 @@
+class operations {
+	public static void main(String[] args){
+		int a = 10;
+		int b = 10;
+		
+		System.out.println("Sum is " + (a+b));
+		System.out.println("Difference is " + (a-b));
+		System.out.println("Product is " + a*b);
+		System.out.println("quotient id " + a/b);
+	}
+}
